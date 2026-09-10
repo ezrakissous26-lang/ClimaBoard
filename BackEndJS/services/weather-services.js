b3c4a83ca4ef2ead15674764a6e9f7959e9fe62a
Ezra <ezrakissous26@gmail.com>
2026-09-10 15:30:35 +0300

    change the structure of fetch function

--- BackEndJS/services/weather-services.js
+++ BackEndJS/services/weather-services.js
@@ -1,25 +1,23 @@
 export async function getCoordinatesByName(cityName) {
     try {
-        const res = await (await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`)).json()
-        return res.results
+        const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`)
+        return await res.json()
     } catch (error) {
-        console.error(error)
-        const err =  new Error(error.message)
+        const err = new Error(error.message)
         err.status = 500
         throw err
     }
 }
-
 
 export async function getWeatherByCoordinates(coordinates) {
 
     const { latitude, longitude } = coordinates
 
     try {
-        const res = await (await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,apparent_temperature,weather_code&daily=temperature_2m_mean,apparent_temperature_mean,wind_speed_10m_max,weather_code`)).json()
-        return res
+        const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,apparent_temperature,weather_code&daily=temperature_2m_mean,apparent_temperature_mean,wind_speed_10m_max,weather_code`)
+        return await res.json()
     } catch (error) {
-        const err =  new Error(error.message)
+        const err = new Error(error.message)
         err.status = 500
         throw err
     }
