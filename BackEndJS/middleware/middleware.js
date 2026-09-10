3eba81c514c9cc60cdc97c8116bfdc26a54fce9b
Ezra <ezrakissous26@gmail.com>
2026-09-10 12:21:38 +0300

    fix checkValidBody middleware

--- BackEndJS/middleware/middleware.js
+++ BackEndJS/middleware/middleware.js
@@ -1,11 +1,14 @@
 export function checkValidBody(req, res, next) {
     if (!req.body || Object.keys(req.body).length === 0) {
         return res.status(400).json({error: 'Body required'})
-    } else if (!req.body.longitude || !req.body.latitude) {
+    } else if (!('latitude' in req.body) || !('longitude' in req.body)) {
         return res.status(400).json({error: 'Invalid body - Fields longitude and latitude are required'})
     } else if (isNaN(Number(req.body.longitude)) || isNaN(Number(req.body.latitude))) {
         return res.status(400).json({error: 'Invalid body - Fields longitude and latitude need be numbers'})
-    } next()
+    } else if ((Number(req.body.longitude) < -180 || Number(req.body.longitude) > 180) || (Number(req.body.latitude) < -90 || Number(req.body.latitude) > 90)) {
+        return res.status(400).json({error: 'Invalid coordonates - Longitude: -180  180 Latitude: -90  90'})
+    }
+    next()
 }
 
 export function logger(req, res, next) {
